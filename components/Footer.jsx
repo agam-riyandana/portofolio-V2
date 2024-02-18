91f898ebf07450e60200d7103c2d124b56d1263a
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-02-19 06:36:01 +0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,23 @@
+import Link from "next/link";
+import Logo from "./Logo";
+import Social from "./Social";
+import { ModeToggle } from "./ModeToggle";
 export default function Footer() {
     return (
-        <footer className="py-7 relative mt-10 px-6 grid gap-2 overflow-x-clip">
-            <div className="flex items-center justify-center gap-2">
-                <a className="p-2 transition bg-secondary text-xs px-3 rounded-full hover:bg-primary hover:text-white" href="https://github.com/r2hu1" target="_blank">Github</a>
-                <a className="p-2 transition bg-secondary text-xs px-3 rounded-full hover:bg-primary hover:text-white" href="https://instagram.com/r.rah_ul" target="_blank">Instagram</a>
+        <footer className="grid gap-3 md:px-20 lg:px-32 px-8 py-10">
+            <div className="flex justify-between">
+                <div>
+                    <Logo />
+                    <p className="text-sm">Powered By<a href="#" className="hover:underline hover:text-primary">AGAM</a><span className="text-primary">.</span></p>
+                </div>
+                <ModeToggle/>
             </div>
-            <p className="text-xs text-center animate-text-gradient bg-gradient-to-r from-neutral-900 via-slate-500 to-neutral-500 bg-[200%_auto] bg-clip-text text-transparent dark:from-neutral-100 dark:via-slate-400 dark:to-neutral-400">Built with <span className="text-red-500">❤️</span> by <a href="https://github.com/r2hu1" className="hover:text-primary">r2hu1</a><span className="text-primary">.</span></p>
-            <div className="absolute inset-0 rotate-180 -z-10 h-[200px] -mt-32 w-full bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:34px_34px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[200px] w-[200px] rounded-full bg-primary opacity-15 blur-[100px]"></div></div>
+            <Social />
+            <ul className="text-sm grid gap-1">
+                <li className="hover:underline hover:text-primary"><Link href="#">Fix this web</Link></li>
+                <li className="hover:underline hover:text-primary"><Link href="#">Report Issue</Link></li>
+            </ul>
+            <p className="text-xs">Copyright © {new Date().getFullYear()} <a href="https://agamriyandana.my.id" className="hover:underline hover:text-primary">Agam Riyandana</a> All Rights Reserved.</p>
         </footer>
     )
 }
